89d60db97e3884ede59865578c9941f4f2d77a33
Adrysson <adrysson@doity.com.br>
2018-05-22 01:10:41 -0300

    Adicionando conteúdo do header, footer e sidebar

---
+++ HTML_CSS/elements/footer.php
@@ -0,0 +1,30 @@
+<footer class="main-footer">
+  <div class="pull-right hidden-xs">
+    <b>Version</b> 2.4.0
+  </div>
+  <strong>Copyright &copy; 2014-2016 <a href="https://adminlte.io">Almsaeed Studio</a>.</strong> All rights
+  reserved.
+</footer>
+
+</div>
+<!-- ./wrapper -->
+
+<!-- jQuery 3 -->
+<script src="../../vendor/bower_components/jquery/dist/jquery.min.js"></script>
+<!-- Bootstrap 3.3.7 -->
+<script src="../../vendor/bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+<!-- SlimScroll -->
+<script src="../../vendor/bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
+<!-- FastClick -->
+<script src="../../vendor/bower_components/fastclick/lib/fastclick.js"></script>
+<!-- AdminLTE App -->
+<script src="../../vendor/dist/js/adminlte.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="../../vendor/dist/js/demo.js"></script>
+<script>
+$(document).ready(function () {
+  $('.sidebar-menu').tree()
+})
+</script>
+</body>
+</html>
