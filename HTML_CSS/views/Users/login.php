cfcea82ab35cdd1e9157d2b396623a6adac2a109
Adrysson <adrysson@doity.com.br>
2018-05-22 01:47:56 -0300

    Login e Cadastro finalizados

--- HTML_CSS/views/Users/login.php
+++ HTML_CSS/views/Users/login.php
@@ -1,53 +1,49 @@
-<?php
-require_once "../../elements/header.php";
-require_once "../../elements/sidebar.php";
-?>
+<?php require_once "../../elements/default/header.php" ?>
 
-<!-- Content Wrapper. Contains page content -->
-<div class="content-wrapper">
-  <!-- Content Header (Page header) -->
-  <section class="content-header">
-    <h1>
-      Blank page
-      <small>it all starts here</small>
-    </h1>
-    <ol class="breadcrumb">
-      <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-      <li><a href="#">Examples</a></li>
-      <li class="active">Blank page</li>
-    </ol>
-  </section>
+<body class="hold-transition login-page">
+<div class="login-box">
+  <div class="login-logo">
+    <a href="#"><b>Teacher</b>Bot</a>
+  </div>
+  <!-- /.login-logo -->
+  <div class="login-box-body">
+    <p class="login-box-msg">Faça seu login</p>
 
-  <!-- Main content -->
-  <section class="content">
-
-    <!-- Default box -->
-    <div class="box">
-      <div class="box-header with-border">
-        <h3 class="box-title">Title</h3>
-
-        <div class="box-tools pull-right">
-          <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip"
-                  title="Collapse">
-            <i class="fa fa-minus"></i></button>
-          <button type="button" class="btn btn-box-tool" data-widget="remove" data-toggle="tooltip" title="Remove">
-            <i class="fa fa-times"></i></button>
+    <form action="login.php" method="post">
+      <div class="form-group has-feedback">
+        <input type="email" class="form-control" placeholder="Email">
+        <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+      </div>
+      <div class="form-group has-feedback">
+        <input type="password" class="form-control" placeholder="Senha">
+        <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+      </div>
+      <div class="row">
+        <div class="col-xs-8">
+          <div class="checkbox icheck">
+            <label>
+              <input type="checkbox"> Lembrar-me
+            </label>
+          </div>
+        </div>
+        <div class="col-xs-4">
+          <button type="submit" class="btn btn-primary btn-block btn-flat">Entrar</button>
         </div>
       </div>
-      <div class="box-body">
-        Start creating your amazing application!
-      </div>
-      <!-- /.box-body -->
-      <div class="box-footer">
-        Footer
-      </div>
-      <!-- /.box-footer-->
+    </form>
+
+    <div class="social-auth-links text-center">
+      <p>- OU -</p>
+      <a href="#" class="btn btn-block btn-social btn-facebook btn-flat"><i class="fa fa-facebook"></i> Entre usando
+        Facebook</a>
+      <a href="#" class="btn btn-block btn-social btn-google btn-flat"><i class="fa fa-google-plus"></i> Entre usando
+        Google+</a>
     </div>
-    <!-- /.box -->
 
-  </section>
-  <!-- /.content -->
+    <a href="#">Perdi minha senha</a><br>
+    Não possui cadastro? <a href="register.php" class="text-center">Cadastre-se gratuitamente</a>
+
+  </div>
 </div>
-<!-- /.content-wrapper -->
 
-<?php require_once "../../elements/footer.php" ?>
+<?php require_once "../../elements/default/footer.php" ?>
--- HTML_CSS/views/Users/register.php
+++ HTML_CSS/views/Users/register.php
@@ -1,6 +1,56 @@
-<?php
-require_once "../../elements/header.php";
-require_once "../../elements/sidebar.php";
-?>
+<?php require_once "../../elements/default/header.php" ?>
 
-<?php require_once "../../elements/footer.php" ?>
+<body class="hold-transition register-page">
+<div class="register-box">
+  <div class="register-logo">
+    <a href="#"><b>Teacher</b>Bot</a>
+  </div>
+
+  <div class="register-box-body">
+    <p class="login-box-msg">Digite suas informações</p>
+
+    <form action="../../index.html" method="post">
+      <div class="form-group has-feedback">
+        <input type="text" class="form-control" placeholder="Nome completo">
+        <span class="glyphicon glyphicon-user form-control-feedback"></span>
+      </div>
+      <div class="form-group has-feedback">
+        <input type="email" class="form-control" placeholder="Email">
+        <span class="glyphicon glyphicon-envelope form-control-feedback"></span>
+      </div>
+      <div class="form-group has-feedback">
+        <input type="password" class="form-control" placeholder="Senha">
+        <span class="glyphicon glyphicon-lock form-control-feedback"></span>
+      </div>
+      <div class="form-group has-feedback">
+        <input type="password" class="form-control" placeholder="Repita sua senha">
+        <span class="glyphicon glyphicon-log-in form-control-feedback"></span>
+      </div>
+      <div class="row">
+        <div class="col-xs-8">
+          <div class="checkbox icheck">
+            <label>
+              <input type="checkbox"> Eu aceito os <a href="#">Termos</a>
+            </label>
+          </div>
+        </div>
+        <!-- /.col -->
+        <div class="col-xs-4">
+          <button type="submit" class="btn btn-primary btn-block btn-flat">Cadastrar</button>
+        </div>
+      </div>
+    </form>
+
+    <div class="social-auth-links text-center">
+      <p>- OU -</p>
+      <a href="#" class="btn btn-block btn-social btn-facebook btn-flat"><i class="fa fa-facebook"></i> Cadastre-se usando
+        Facebook</a>
+      <a href="#" class="btn btn-block btn-social btn-google btn-flat"><i class="fa fa-google-plus"></i> Cadastre-se usando
+        Google+</a>
+    </div>
+
+    Já possui conta? <a href="login.php" class="text-center">Realize seu login</a>
+  </div>
+</div>
+
+<?php require_once "../../elements/default/footer.php" ?>
