6563e326a265a62a31051df017dcf74c094c068f
Arthur <henrrique1995@gmail.com>
2018-07-12 00:13:38 -0300

    Revert "mudei a localização dos botões de gerenciamento"
    This reverts commit dd1ec44e609e4db96c05d19377d646ce9a2ad429.

--- LARAVEL/resources/views/home.blade.php
+++ LARAVEL/resources/views/home.blade.php
@@ -15,7 +15,19 @@
                     @endif
                     <div class="row">
                         <div class="col-md-12" align="center">
-                            <a href="{{ url('/bots') }}" class="btn btn-success btn-lg btn-block">Gerenciar</a>
+                            <a href="{{ url('/bots') }}" class="btn btn-success btn-lg btn-block">Gerenciar Bots</a>
+                        </div>
+                    </div>
+                    <br>
+                    <div class="row">
+                        <div class="col-md-4" align="center">
+                            <a href="{{ url('/exams') }}" class="btn btn-danger btn-lg btn-block">Gerenciar Provas</a>
+                        </div>
+                        <div class="col-md-4">
+                            <a href="{{ url('/resources') }}" class="btn btn-primary btn-lg btn-block">Gerenciar Materiais</a>
+                        </div>
+                        <div class="col-md-4">
+                            <a href="{{ url('/questions') }}" class="btn btn-warning btn-lg btn-block">Gerenciar Questões</a>
                         </div>
                     </div>
                 </div>
